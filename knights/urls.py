1e2514409c22eec073e37f26fecd916c7322b8ce
Tamer Wahba <tamwahba@hotmail.com>
2014-02-06 23:48:22 -0500

    Added friends

--- knights/urls.py
+++ knights/urls.py
@@ -6,10 +6,12 @@ urlpatterns = patterns('',
                        url(r'^registeruser/', views.registeruser, name='registeruser'),
                        # /knights/loginuser/
                        url(r'^loginuser/', views.loginuser, name='loginuser'),
-                       # /knights/<user_name>/returnspriteinfo/
-                       url(r'^(?P<user_name>[-A-Za-z0-9_]+)/returnspriteinfo/$', views.returnspriteinfo, name='returnspriteinfo'),
                        # /knights/highscores/
                        url(r'^highscores/', views.highscores, name='highscores'),
                        # /knights/<user_name>/updateuser/
                        url(r'^(?P<user_name>[-A-Za-z0-9_]+)/updateuser/', views.updateuser, name='updateuser'),
+                       # /knights/<user_name>/returnspriteinfo/
+                       url(r'^(?P<user_name>[-A-Za-z0-9_]+)/returnspriteinfo/$', views.returnspriteinfo, name='returnspriteinfo'),
+                       # /knights/<user_name>/friends
+                       url(r'^(?P<user_name>[-A-Za-z0-9_]+)/friends', views.friends, name='friends'),
                        )
